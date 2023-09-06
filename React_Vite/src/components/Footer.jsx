1d938c3d4adfbe9789eeb0c8fa9ac5dcf1e8d582
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-09-06 16:15:32 +0200

    fix footer no sticky

--- React_Vite/src/components/Footer.jsx
+++ React_Vite/src/components/Footer.jsx
@@ -6,37 +6,32 @@ export const Footer = () => {
   const isNotFoundPage = location.pathname === "/404";
 
   if (!isNotFoundPage) {
-    return (  
-    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023 
-            <a href="https://github.com/JiesGame" target="_blank" rel="noopener noreferrer" className="hover:underline">
-              JiesGame
-            </a>
-          . All Rights Reserved.
-        </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6">
-                  About
+    return ( 
+    <footer class="bg-gray-100 w-full absolute">
+        <div class="w-full p-4">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <a href="https://github.com/" class="flex items-center mb-4 sm:mb-0">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" class="h-8 mr-3" alt="GitHub Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">JiesGame</span>
                 </a>
-            </li>
-            <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6">
-                  Privacy Policy
-                </a>
-            </li>
-            <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6">
-                  Licensing
-                </a>
-            </li>
-            <li>
-                <a href="#" className="hover:underline">
-                  Contact
-                </a>
-            </li>
-        </ul>
+                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://github.com/JiesGame" class="hover:underline">JiesGame™</a>. All Rights Reserved.</span>
+        </div>
     </footer>
     );
   }
